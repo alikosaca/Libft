f5878a4e213538ef0c15759753735be951a628a2
Ali Abbas Koşaca <akosaca@k1m11s03.42istanbul.com.tr>
2024-12-02 17:42:56 +0300

    updating

---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 15:54:18 by akosaca           #+#    #+#             */
+/*   Updated: 2024/10/29 15:11:02 by akosaca          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+	size_t			i;
+
+	str = (unsigned char *)s;
+	i = 0;
+	while (n > 0)
+	{
+		if (str[i] == (unsigned char)c)
+			return ((unsigned char *)&s[i]);
+		i++;
+		n--;
+	}
+	return (NULL);
+}
